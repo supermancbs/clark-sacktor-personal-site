66940f37f38ddc2bd16a00e9ea37c29e668e24ed
supermancbs <clark.sacktor@gmail.com>
2016-09-09 16:18:03 -0400

    add carasel

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react'
+import { Carousel } from 'react-bootstrap'
 
 class Portfolio extends Component{
   render(){
     return(
-      <div>
-    Clark's projects
+      <Carousel>
+    <Carousel.Item>
+      <img width={`100%`} height={`100%`} alt="900x500" src="http://learn-co-students.github.io/deploy-on-day-1-web-0616/img/students/clark_sacktor_profile.jpg"/>
+      <Carousel.Caption>
+        <h3>First slide label</h3>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img width={`100%`} height={`100%`} alt="900x500" src="../images/png/FeedMe.png"/>
+      <Carousel.Caption>
+        <h3>Second slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img width={900} height={500} alt="900x500" src="/assets/carousel.png"/>
+      <Carousel.Caption>
+        <h3>Third slide label</h3>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+  </Carousel>
 
-
-      </div>
     )
   }
 }
